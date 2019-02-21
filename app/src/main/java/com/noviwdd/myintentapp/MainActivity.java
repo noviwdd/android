ff8a95171c6d8bff1066ac8388ef4c95e6f63549
noviwdd <noviwdd2@gmail.com>
2019-02-21 11:51:09 +0700

    first upload

---
+++ app/src/main/java/com/noviwdd/myintentapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.noviwdd.myintentapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import static com.noviwdd.myintentapp.R.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/noviwdd/myintentapp/MainActivityImpl.java
@@ -0,0 +1,12 @@
+package com.noviwdd.myintentapp;
+
+/**
+ * Created by Asus A456U on 12/27/2017.
+ */
+
+class MainActivityImpl extends MainActivity {
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+}
